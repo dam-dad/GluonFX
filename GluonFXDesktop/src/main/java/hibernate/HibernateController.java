5f81110cf25ffc695b7117373a9e052c51f23ec9
moimah <moimah@outlook.com>
2020-01-19 21:06:46 +0000

    utils ok

---
+++ GluonFXDesktop/src/main/java/hibernate/HibernateController.java
@@ -0,0 +1,122 @@
+package hibernate;
+
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
+
+
+public class HibernateController {
+	
+
+		private StandardServiceRegistry sr;
+		private SessionFactory sf;
+		private Session session;
+
+		/*
+		 * Inicializa la conexion
+		 */
+		public void start() {
+
+			//Inicialización del SessionFactory
+			sr = new StandardServiceRegistryBuilder().configure().build();		
+					
+			sf = new MetadataSources(sr).buildMetadata().buildSessionFactory();				
+							
+		    //Apertura de una sesión (e inicio de una transacción)		
+		    session = sf.openSession();
+			
+
+		}
+		
+		
+		public void close() {
+			
+			session.close();
+		}
+		
+		
+		/*
+		 * Guarda un objeto
+		 */
+		public void save(Object object) {
+			
+					
+				session.clear();
+				
+				session.beginTransaction();
+				
+				session.save(object);
+						
+				session.getTransaction().commit();
+		
+			
+		}
+		
+				
+		
+		/*
+		 * Actualiza un objeto
+		 */
+		
+		public void update (Object object) {
+			
+			
+			
+			session.clear();
+			
+			session.beginTransaction();
+			
+			session.update(object);
+			
+			session.getTransaction().commit();
+		}
+		
+		
+		/*
+		 * Elimina un objeto
+		 */
+		
+		public void delete (Object object) {
+			
+			//session.clear();
+			
+			session.beginTransaction();
+			
+			session.delete(object);
+			
+			session.getTransaction().commit();
+		}
+		
+		
+		/*
+		 * Selecciona todos los elementos de una lista concreta
+		 */		
+		public List<?> selectAll(String entityName) {
+			
+			
+			session.clear();
+			
+			session.beginTransaction();
+			
+			Query<?> query = session.createQuery("From " + entityName);
+			
+			
+			List<?> lista = query.list();
+			
+			session.getTransaction().commit();
+					
+			return lista; 				
+			
+			
+		}
+		
+
+		
+
+}
---
+++ GluonFXDesktop/src/main/java/test/TestEntities.java
@@ -0,0 +1,33 @@
+package test;
+
+import java.util.ArrayList;
+
+import entities.Company;
+import hibernate.HibernateController;
+
+public class TestEntities {
+	
+	public static void main(String[] args) {
+		
+		HibernateController hibernate = new HibernateController();		
+		hibernate.start();
+		
+		
+		//Test insert company		
+		Company c1 = new Company();
+		c1.setCompanyId("44444");
+		c1.setCity("La Laguna");
+		
+	//	hibernate.save(c1);
+		
+		ArrayList<Company> list = (ArrayList<Company>) hibernate.selectAll("Company");
+		
+		for (int i = 0; i <list.size(); i++) {
+			
+			System.out.println(list.get(i).getCompanyId());
+			
+		}
+	}
+	
+
+}
