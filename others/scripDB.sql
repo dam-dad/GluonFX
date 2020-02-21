88275b545809ec33b17870456f3271fb65c8412e
blackbacil <55440126+blackbacil@users.noreply.github.com>
2020-02-21 11:48:26 +0000

    Update scripDB.sql

--- others/scripDB.sql
+++ others/scripDB.sql
@@ -179,53 +179,53 @@ CONSTRAINT fk_work_order_concept FOREIGN KEY (work_order_id) REFERENCES work_ord
 
 DELIMITER //
 CREATE TRIGGER insert_concept_invoice BEFORE INSERT ON concept_invoice
-	FOR EACH ROW
-      	BEGIN
-      	      UPDATE invoice SET price = ((SELECT price FROM invoice WHERE id = NEW.invoice_id) + (NEW.price)) WHERE id = NEW.invoice_id;
+      FOR EACH ROW
+            BEGIN
+                  UPDATE invoice SET price = ((SELECT price FROM invoice WHERE id = NEW.invoice_id) + (NEW.price)) WHERE id = NEW.invoice_id;
                   UPDATE invoice SET tax_total = ((SELECT price FROM invoice WHERE id = NEW.invoice_id) * (SELECT percentage FROM tax WHERE id = (SELECT tax_id FROM invoice WHERE id = NEW.invoice_id)) / 100) WHERE id = NEW.invoice_id;
                   UPDATE invoice SET price_taxes_included = ((SELECT price + tax_total FROM invoice WHERE id = NEW.invoice_id)) WHERE id = NEW.invoice_id;
-      	END;
+            END;
 //
 DELIMITER ;
 
 DELIMITER //
 CREATE TRIGGER insert_concept_work_order BEFORE INSERT ON concept_work_order
-	FOR EACH ROW
-      	BEGIN
+      FOR EACH ROW
+            BEGIN
                   UPDATE work_order SET price = ((SELECT price FROM work_order WHERE id = NEW.work_order_id) + (NEW.price)) WHERE id = NEW.work_order_id;
                   UPDATE work_order SET tax_total = ((SELECT price FROM work_order WHERE id = NEW.work_order_id) * (SELECT percentage FROM tax WHERE id = (SELECT tax_id FROM work_order WHERE id = NEW.work_order_id)) / 100) WHERE id = NEW.work_order_id;
                   UPDATE work_order SET price_taxes_included = ((SELECT price + tax_total FROM work_order WHERE id = NEW.work_order_id)) WHERE id = NEW.work_order_id;
-      	END;
+            END;
 //
 DELIMITER ;
 
 DELIMITER //
 CREATE TRIGGER insert_concept_budget BEFORE INSERT ON concept_budget
-	FOR EACH ROW
-      	BEGIN
-      		UPDATE budget SET price = ((SELECT price FROM budget WHERE id = NEW.budget_id) + (NEW.price)) WHERE id = NEW.budget_id;
+      FOR EACH ROW
+            BEGIN
+                  UPDATE budget SET price = ((SELECT price FROM budget WHERE id = NEW.budget_id) + (NEW.price)) WHERE id = NEW.budget_id;
                   UPDATE budget SET tax_total = ((SELECT price FROM budget WHERE id = NEW.budget_id) * (SELECT percentage FROM tax WHERE id = (SELECT tax_id FROM budget WHERE id = NEW.budget_id)) / 100) WHERE id = NEW.budget_id;
                   UPDATE budget SET price_taxes_included = ((SELECT price + tax_total FROM budget WHERE id = NEW.budget_id)) WHERE id = NEW.budget_id;
-      	END;
+            END;
 //
 DELIMITER ;
 
 DELIMITER //
 CREATE TRIGGER insert_invoice_number BEFORE INSERT ON invoice
-	FOR EACH ROW
-      	BEGIN
-      		SET NEW.invoice_number = (SELECT CONCAT_WS('',(SELECT YEAR(NEW.invoice_date)), (SELECT LPAD((MONTH(NEW.invoice_date)),2,'0')), (SELECT LPAD((SELECT COUNT(invoice_number) + 1  FROM invoice WHERE YEAR(invoice_date) = YEAR(NEW.invoice_date) AND MONTH(invoice_date) = MONTH(NEW.invoice_date)),4,'0'))));
-      	END;
+      FOR EACH ROW
+            BEGIN
+                  SET NEW.invoice_number = (SELECT CONCAT_WS('',(SELECT YEAR(NEW.invoice_date)), (SELECT LPAD((MONTH(NEW.invoice_date)),2,'0')), (SELECT LPAD((SELECT COUNT(invoice_number) + 1  FROM invoice WHERE YEAR(invoice_date) = YEAR(NEW.invoice_date) AND MONTH(invoice_date) = MONTH(NEW.invoice_date)),4,'0'))));
+            END;
 //
 DELIMITER ;
 
 
 DELIMITER //
 CREATE TRIGGER insert_budget_number BEFORE INSERT ON budget
-	FOR EACH ROW
-      	BEGIN
-      		SET NEW.budget_number = (SELECT CONCAT_WS('','B',(SELECT YEAR(NEW.budget_date)), (SELECT LPAD((MONTH(NEW.budget_date)),2,'0')), (SELECT LPAD((SELECT COUNT(budget_number) + 1  FROM budget WHERE YEAR(budget_date) = YEAR(NEW.budget_date) AND MONTH(budget_date) = MONTH(NEW.budget_date)),4,'0'))));
-      	END;
+      FOR EACH ROW
+            BEGIN
+                  SET NEW.budget_number = (SELECT CONCAT_WS('','B',(SELECT YEAR(NEW.budget_date)), (SELECT LPAD((MONTH(NEW.budget_date)),2,'0')), (SELECT LPAD((SELECT COUNT(budget_number) + 1  FROM budget WHERE YEAR(budget_date) = YEAR(NEW.budget_date) AND MONTH(budget_date) = MONTH(NEW.budget_date)),4,'0'))));
+            END;
 //
 DELIMITER ;
 
@@ -233,10 +233,10 @@ DELIMITER ;
 
 DELIMITER //
 CREATE TRIGGER insert_work_order_number BEFORE INSERT ON work_order
-	FOR EACH ROW
-      	BEGIN
+      FOR EACH ROW
+            BEGIN
                   SET NEW.work_order_number = (SELECT CONCAT_WS('','WO',(SELECT YEAR(NEW.work_order_date)), (SELECT LPAD((MONTH(NEW.work_order_date)),2,'0')), (SELECT LPAD((SELECT COUNT(work_order_number) + 1  FROM work_order WHERE YEAR(work_order_date) = YEAR(NEW.work_order_date) AND MONTH(work_order_date) = MONTH(NEW.work_order_date)),4,'0'))));
-      	END;
+            END;
 //
 DELIMITER ;
 
@@ -409,32 +409,40 @@ DELIMITER ;
 
 USE 7057507_administration_db;
 
-INSERT INTO company (company_id, name, address, city, country, email, phone) VALUES ('D0001', 'MyCompany', 'C/pincel 12','la laguna', 'Spain', 'info@mycompany.es', '686868');
+INSERT INTO company (company_id, name, address, city, country, email, phone) VALUES ('D0001', 'Informatica SL', 'C/pincel 12','la laguna', 'Spain', 'inforsl@mycompany.es', '922686868');
 
-INSERT INTO customer (customer_id, name, address, city, country, email, phone) VALUES ('A0001', 'Amazon', 'C/Peliaguda 11','la laguna', 'Spain', 'info@amazon.es', '6333333');
+INSERT INTO customer (customer_id, name, address, city, country, email, phone) VALUES ('A0001', 'Tu bicicleta ya', 'C/Peliaguda 11','la laguna', 'Spain', 'info@tubici.es', '9226333333');
 
-INSERT INTO tax (tax_id, percentage, description) VALUES ('IGIC', 7, 'IMPUESTO CANARIO BASE'); 
+INSERT INTO tax (tax_id, percentage, description) VALUES ('IGIC', 7, 'IMPUESTO CANARIO INDIRECTO GENERAL'); 
 
-INSERT INTO tax (tax_id, percentage, description) VALUES ('IVA', 21, 'IMPUESTO GODO'); 
+INSERT INTO tax (tax_id, percentage, description) VALUES ('IVA', 21, 'IMPUESTO VALOR AÑADIDO'); 
 
 INSERT INTO pay_method(description) VALUES ('Tansferencia'); 
 
 INSERT INTO pay_method(description) VALUES ('Efectivo'); 
 
-INSERT INTO pay_method(description) VALUES ('Cr�dito'); 
-
-INSERT INTO product (product_id, name, description, price, stock, url) VALUES ('10000191', 'Consolador', 'placentero y peque�o', 9.90, 900, 'www.pornhub.com');
+INSERT INTO pay_method(description) VALUES ('Crédito'); 
+INSERT INTO product (product_id, name, description, price, stock, url) VALUES ('10000191', 'ASUS M5A78L-M PLUS/USB3 - 90MB0RB0-M0EAY0', 'Placa base ASUS', 110.6, 900, 'https://i.imgur.com/ZKMDR9i.jpeg');
+INSERT INTO product (product_id, name, description, price, stock, url) VALUES ('10000192', 'ROG Pugio', 'Raton ASUS', 60.6, 900, 'https://i.imgur.com/jncB7bp.jpg');
+INSERT INTO product (product_id, name, description, price, stock, url) VALUES ('10000193', 'Impresora multifunción Canon Pixma TS3150', 'Impresora multifuncion', 100.6, 100, 'https://i.imgur.com/1yvSued.jpg');
+INSERT INTO product (product_id, name, description, price, stock, url) VALUES ('10000194', 'HP 22w - Monitor 21.5" ', 'Full HD, 1920 x 1080 pixeles, tiempo de respuesta de 5 ms, 1 x HDMI, 1 x VGA, 16:9', 180.6, 100, 'https://i.imgur.com/xCDupm6.jpeg');
+INSERT INTO product (product_id, name, description, price, stock, url) VALUES ('10000195', 'HyperX Fury HX426C16FB3AK2/16 Memoria RAM DIMM DDR4 ', '16GB 2666MHz CL161Rx8 RGB', 120.6, 100, 'https://i.imgur.com/wPVshiO.png');
+INSERT INTO product (product_id, name, description, price, stock, url) VALUES ('10000196', 'Procesador Intel® Core™ i9-9900K', '16 MB de caché, hasta 5 GHz', 500.6, 100, 'https://i.imgur.com/aYGDDnJ.jpeg');
+INSERT INTO product (product_id, name, description, price, stock, url) VALUES ('10000197', 'Limpieza', ' limpieza del pc y cambiamos pasta termica', 50.6, 100000, '');
+INSERT INTO product (product_id, name, description, price, stock, url) VALUES ('10000198', 'Mano de obra', 'Mano de obra de un tecnico expeciaizado', 30.0, 100000, '');
+INSERT INTO product (product_id, name, description, price, stock, url) VALUES ('10000198', 'Formateo', 'Te hacemos una tarea que podrias hacer tu solo y te cobramos muchisimo por ello gracias', 50.6, 100000, '');
+INSERT INTO product (product_id, name, description, price, stock, url) VALUES ('10000199', 'Transporte', 'Si es necesario transporte del tecnico', 10.6, 100000, '');
 
 INSERT INTO invoice (company_id, customer_id, invoice_date, concept_id, pay_method_id, tax_id) VALUES (1, 1, '2020-01-20', 1, 1, 1);
 
 INSERT INTO invoice (company_id, customer_id, invoice_date, concept_id, pay_method_id, tax_id) VALUES (1, 1, '2020-01-20', 1, 1, 1);
 
-INSERT INTO concept_invoice (invoice_id,description,price) VALUES (1,'kit de placer',300); 
+INSERT INTO concept_invoice (invoice_id,description,price) VALUES (1,'Presupuesto de un ordenador sobremesa',30); 
 
 INSERT INTO invoice_detail (invoice_id, product_id, quantity) VALUES (1, 1, 3);
 
 INSERT INTO invoice_detail (invoice_id, product_id, quantity) VALUES (2, 1, 10);
 
-INSERT INTO product (product_id, name, description, price,stock, url) VALUES ('10000192', 'Consolador', 'placentero y peque�o', 1.90,700, 'www.pornhub.com');
+
 
 INSERT INTO invoice_detail (invoice_id, product_id, quantity) VALUES (1, 2, 8);
